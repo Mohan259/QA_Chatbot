03cc969fe141c4d5191ec982088fd6d7a465764c
Mohan <selvamk1249@gmail.com>
2024-05-28 21:36:45 +0530

    created basic version- url seach chatbot

---
+++ app.py
@@ -0,0 +1,65 @@
+from langchain_openai import ChatOpenAI,OpenAIEmbeddings
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.vectorstores import FAISS
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+import os
+import streamlit as st
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+# Get LLM
+llm = ChatOpenAI(model="gpt-3.5-turbo",temperature=0.5,openai_api_key=os.getenv("OPENAI_API_KEY"))
+print(llm)
+
+# ChatPromptTemplate
+prompt = ChatPromptTemplate.from_template(
+"""
+Answer the given question only based on the given context.
+Please provide the most accurate response based on the question
+<context>
+{context}
+<context>
+                                 
+Question: {input}
+""")
+
+chain = create_stuff_documents_chain(llm=llm,prompt=prompt)
+
+
+st.title("Q&A Chatbot")
+
+url = st.text_input("Enter the URL...")
+url_ques = st.text_input("Enter your question...")
+
+wiki_ques = st.text_input("Enter your question to search in wikipedia...")
+
+if url and url_ques:
+    if "URL" not in st.session_state:
+        st.session_state.loader = WebBaseLoader(web_path=url)
+        st.session_state.docs = st.session_state.loader.load()
+        st.session_state.splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+        st.session_state.url_docs = st.session_state.splitter.split_documents(documents=st.session_state.docs)
+        st.session_state.embeddings = OpenAIEmbeddings()
+        st.session_state.url_db = FAISS.from_documents(documents=st.session_state.url_docs,embedding=st.session_state.embeddings)
+
+    retriver_url = st.session_state.url_db.as_retriever()
+
+    # Combine the retriver and chain
+
+    retrieval_chain_url = create_retrieval_chain(retriver_url,chain)
+    response = retrieval_chain_url.invoke({'input':url_ques})
+    st.write(response['answer'])
+
+
+
+
+
+
+
+
+
