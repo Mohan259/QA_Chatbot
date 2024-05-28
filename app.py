c2492c7ab569c2d21deb5349d1f63c6338d594f3
Mohan <selvamk1249@gmail.com>
2024-05-28 22:04:18 +0530

    added feature for wiki search

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.utilities import WikipediaAPIWrapper
+from langchain_community.tools import WikipediaQueryRun
+from langchain.agents import create_openai_tools_agent
+from langchain.agents import AgentExecutor
+from langchain import hub
 import os
 import streamlit as st
 from dotenv import load_dotenv
@@ -39,7 +44,7 @@ url_ques = st.text_input("Enter your question...")
 wiki_ques = st.text_input("Enter your question to search in wikipedia...")
 
 if url and url_ques:
-    if "URL" not in st.session_state:
+    if "url_db" not in st.session_state:
         st.session_state.loader = WebBaseLoader(web_path=url)
         st.session_state.docs = st.session_state.loader.load()
         st.session_state.splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
@@ -55,7 +60,23 @@ if url and url_ques:
     response = retrieval_chain_url.invoke({'input':url_ques})
     st.write(response['answer'])
 
+# Use of Agents
+if wiki_ques:
+    api_wrapper = WikipediaAPIWrapper(top_k_results=1,doc_content_chars_max=200)
+    wiki = WikipediaQueryRun(api_wrapper=api_wrapper)
 
+    # Can add many agents
+    tools = [wiki]
+
+
+    agent_prompt = hub.pull("hwchase17/openai-functions-agent")
+
+    agent = create_openai_tools_agent(llm,tools,agent_prompt)
+    agent_executor = AgentExecutor(agent=agent,tools=tools,verbose=True)
+
+    agent_ans = agent_executor.invoke({'input':wiki_ques})
+    st.write(agent_ans["output"])
+    
 
 
 
